c324db453a7cf0ec63bf197bd1fbcd47e571905e
USER <marielashtein@gmail.com>
2025-11-14 15:00:56 +0200

    start register

--- app/src/main/java/com/example/becomingfamily/LoginActivity.java
+++ app/src/main/java/com/example/becomingfamily/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.becomingfamily;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +37,37 @@ public class LoginActivity extends AppCompatActivity {
         fabLogin=findViewById(R.id.fabLogin);
         currentData=new CurrentData();
     }
+    public boolean verifyData()
+    {
+        Log.d("MARIELA","verifyData");
+        if (etEmail.getText().toString().trim().isEmpty()) {            etEmail.setError("הזן דואל");
+            return false;
+        }
+        if (etPassword.getText().toString().trim().isEmpty())
+        {
+            etPassword.setError("הזן סיסמה");
+            return false;
+        }
+
+        return true;
+    }
+    public CurrentData GetCurrentData(String email)
+    {
+        currentData.SetEmail(email);
+        //verify dbs correct weeks-get last period and count weeks
+        currentData.SetWeeks(1);
+
+        return  currentData;
+    }
+
+    public void SaveCurrentData(CurrentData currentData)
+    {
+        SharedPreferences sp=getSharedPreferences("BabySteps",MODE_PRIVATE);
+        SharedPreferences.Editor editor= sp.edit();
+        editor.putString("email",currentData.GetEmail());
+        editor.putInt("weeks",currentData.GetWeeks());
+        editor.commit();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +76,33 @@ public class LoginActivity extends AppCompatActivity {
         init();
 
 
+        fabLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=etEmail.getText().toString();
+                String password=etPassword.getText().toString();
+                Log.d("MARIELA","Login "+email);
+                if (verifyData()) {
+                    Auth.signIn(LoginActivity.this, email, password, task -> {
+                        if (task.isSuccessful()) {
+                            currentData = GetCurrentData(email);
+                            SaveCurrentData(currentData);
+
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, TrackActivity.class);
+                            startActivity(intent);
+                        } else {
+                            etEmail.setError(task.getException().toString());
+                            etPassword.setError(task.getException().toString());
+                            Toast.makeText(LoginActivity.this, "Login Failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Login Failed. " , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
