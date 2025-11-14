3feef39d3c69a200dd2848024f8781165a9f91c8
USER <marielashtein@gmail.com>
2025-11-14 20:35:10 +0200

    tracker activity

--- app/src/main/java/com/example/becomingfamily/TrackActivity.java
+++ app/src/main/java/com/example/becomingfamily/TrackActivity.java
@@ -1,6 +1,8 @@
 package com.example.becomingfamily;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TrackActivity extends AppCompatActivity {
-
+private TextView tv_selected_date;
+private Button btn_calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_track);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.llTrack), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
